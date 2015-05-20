2ea13cf0959f58d6741f42aa438e33c10bb020f7
ag12 <amirg12@hotmail.no>
2015-05-20 20:14:06 +0200

    Initial Commit

---
+++ HttpWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HttpWeather
+//
+//  Created by Amir Ghoreshi on 20/05/15.
+//  Copyright (c) 2015 AM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HttpWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpWeather
+//
+//  Created by Amir Ghoreshi on 20/05/15.
+//  Copyright (c) 2015 AM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
